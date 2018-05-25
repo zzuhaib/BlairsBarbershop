e815f1796fca68404ffa78863c2871f834ddc1c2
zzuhaib <38409714+zzuhaib@users.noreply.github.com>
2018-05-24 21:46:45 -0400

    Using DatePicker to select a date

---
+++ app/src/main/java/com/example/zuhaib/blairsbarbershop/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.zuhaib.blairsbarbershop;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private TextView textViewDate;
+    private DatePickerDialog.OnDateSetListener mDisplaySetListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewDate = (Button) findViewById(R.id.button_date);
+
+        textViewDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(MainActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDisplaySetListener, year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDisplaySetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month++;
+                String date = month + "/" + dayOfMonth + "/" + year;
+                textViewDate.setText(date);
+            }
+        };
+    }
+}
