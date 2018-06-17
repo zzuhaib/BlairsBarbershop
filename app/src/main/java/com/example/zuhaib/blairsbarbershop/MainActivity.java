d7709b87c60b02dc8ff3db2d4b684edb6f2fe6c0
zzuhaib <38409714+zzuhaib@users.noreply.github.com>
2018-06-17 01:17:15 -0400

    CustomerTimePicker for time intervals Google Calendar Integration New Icons New font

--- app/src/main/java/com/example/zuhaib/blairsbarbershop/MainActivity.java
+++ app/src/main/java/com/example/zuhaib/blairsbarbershop/MainActivity.java
@@ -3,15 +3,20 @@ package com.example.zuhaib.blairsbarbershop;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -19,23 +24,10 @@ import android.widget.Toast;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.DateTime;
-import com.google.api.services.calendar.model.Event;
-import com.google.api.services.calendar.model.EventAttendee;
-import com.google.api.services.calendar.model.EventDateTime;
-import com.google.api.services.calendar.model.EventReminder;
-
 import org.json.JSONObject;
+import org.mortbay.jetty.Main;
 
-import java.io.IOException;
-import java.sql.Time;
-import java.text.DateFormat;
-import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.text.DateFormatSymbols;
 import java.util.Date;
@@ -47,9 +39,11 @@ public class MainActivity extends AppCompatActivity {
     private DatePickerDialog.OnDateSetListener mDisplaySetListener;
     private EditText editTextName;
     private TextView textViewCalendarEvent;
+    private ImageView imageViewInstagram;
+    private ImageView imageViewInfo;
     private Button submitButton;
     String name, date, time;
-    private DateTimeRequest dateTimeRequest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Initialization
@@ -60,7 +54,18 @@ public class MainActivity extends AppCompatActivity {
         editTextName = (EditText) findViewById(R.id.edittext_name);
         submitButton = (Button) findViewById(R.id.button_schedule);
         textViewCalendarEvent = (TextView) findViewById(R.id.textView_GoogleCalendar);
+        imageViewInstagram = (ImageView) findViewById(R.id.imageView_instagram);
+        imageViewInfo = (ImageView) findViewById(R.id.imageView_Info);
 
+        getDateValue();
+        getTimeValue();
+        submitAppointment();
+        clickGoogleCalendarLink();
+        clickInstagramIcon();
+        clickInfoButton();
+    }
+
+    private void getDateValue(){
         //Get Date Value
         textViewDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,39 +87,44 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 month++;
-                String nameMonth = new DateFormatSymbols().getMonths()[month-1];
+                String nameMonth = new DateFormatSymbols().getMonths()[month - 1];
 
                 String date = year + "-" + month + "-" + dayOfMonth;
-                textViewDate.setText(date);
+                textViewDate.setText(" " + date);
             }
         };
+    }
 
+    private void getTimeValue(){
         //Get Time Value
-        textViewTime.setOnClickListener(new View.OnClickListener(){
+        textViewTime.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 Calendar currentTime = Calendar.getInstance();
                 int hour = currentTime.get(Calendar.HOUR_OF_DAY);
                 int minute = currentTime.get(Calendar.MINUTE);
-                TimePickerDialog mTimePicker;
-                mTimePicker = new TimePickerDialog
-                        (MainActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, new TimePickerDialog.OnTimeSetListener(){
+
+
+                CustomTimePickerDialog mTimePicker;
+                mTimePicker = new CustomTimePickerDialog
+                        (MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
                             @Override
-                            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute){
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                                 String am_pm;
-                                if (selectedHour > 12) {
-                                    selectedHour -= 12;
+                                if (hourOfDay > 12) {
+                                    hourOfDay -= 12;
                                     am_pm = "PM";
-                                } else if (selectedHour == 0) {
-                                    selectedHour += 12;
+                                } else if (hourOfDay == 0) {
+                                    hourOfDay += 12;
                                     am_pm = "AM";
-                                } else if (selectedHour == 12){
+                                } else if (hourOfDay == 12) {
                                     am_pm = "PM";
-                                }else{
+                                } else {
                                     am_pm = "AM";
                                 }
-                                String time = String.format("%02d:%02d", selectedHour, selectedMinute);
-                                textViewTime.setText(time + " " + am_pm);
+
+                                String time = String.format("%02d:%02d", hourOfDay, minute);
+                                textViewTime.setText(" " + time + " " + am_pm);
                             }
                         }, hour, minute, false);
                 mTimePicker.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -122,11 +132,13 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    private void submitAppointment(){
         //Submit button to send email to notify barber
-        submitButton.setOnClickListener(new View.OnClickListener(){
+        submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 name = editTextName.getText().toString();
                 date = textViewDate.getText().toString();
                 time = textViewTime.getText().toString();
@@ -137,18 +149,18 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             JSONObject jsonResponse = new JSONObject(response);
                             Boolean success = jsonResponse.getBoolean("success");
-                            if (success){
+                            if (success) {
                                 new SimpleMail().sendEmail("blairsbarbershop@gmail.com", "Hey Blair, " + name +
                                         " scheduled an appointment for " + time + " on " + date, "Be sure to give " + name + " the best haircut possible!");
                                 showToast("Appointment scheduled!");
-                            }else{
+                            } else {
                                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                                 builder.setMessage("This time has already been booked!")
                                         .setNegativeButton("Please try another time", null)
                                         .create()
                                         .show();
                             }
-                        }catch (Exception e){
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
@@ -158,7 +170,28 @@ public class MainActivity extends AppCompatActivity {
                 queue.add(dateTimeRequest);
             }
         });
+    }
 
+    private void clickInstagramIcon(){
+        imageViewInstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.instagram.com/champagnepapi/?hl=en");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.instagram.android");
+
+                try {
+                    startActivity(likeIng);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://www.instagram.com/champagnepapi/?hl=en")));
+                }
+            }
+        });
+    }
+
+    private void clickGoogleCalendarLink(){
         //Set Google Calendar
         textViewCalendarEvent.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -167,20 +200,33 @@ public class MainActivity extends AppCompatActivity {
                 time = textViewTime.getText().toString();
 
                 try {
-                    Date fullDate = new SimpleDateFormat("yyyy-MM-dd-HH:mm a").parse(date+"-"+time);
+                    Date fullDate = new SimpleDateFormat("yyyy-MM-dd-HH:mm a").parse(date + "-" + time);
                     Intent intent = new Intent(Intent.ACTION_EDIT);
                     intent.setType("vnd.android.cursor.item/event");
                     intent.putExtra("beginTime", fullDate.getTime());
                     intent.putExtra("allDay", false);
-                    intent.putExtra("title", "Haircut");
-                    intent.putExtra("endTime", fullDate.getTime()+60*60*1000);
+                    intent.putExtra("title", "Haircut with Bilal");
+                    intent.putExtra("endTime", fullDate.getTime() + 60 * 60 * 1000);
                     startActivity(intent);
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
             }
-
         });
+    }
+
+    private void clickInfoButton(){
+        imageViewInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Bilals Barbershop");
+                builder.setMessage("Hours of Operation: \nMon-Fri: 6pm-11pm \nSat-Sun: 9am-10pm\n\nDeveloped by Zuhaib\nLogo design by KajanKajan\n\nContact us @ blairsbarbershop@gmail.com for any inquiries")
+                        .setNegativeButton("Got it!", null)
+                        .create()
+                        .show();
+            }
+    });
     }
 
     private void showToast(String text){
