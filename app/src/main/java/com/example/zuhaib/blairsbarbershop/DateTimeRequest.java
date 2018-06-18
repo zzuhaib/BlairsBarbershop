7c42066c01e74cf1a282a6f923bffb54ec76c8bd
zzuhaib <38409714+zzuhaib@users.noreply.github.com>
2018-06-17 20:01:16 -0400

    Removed authFailureError

--- app/src/main/java/com/example/zuhaib/blairsbarbershop/DateTimeRequest.java
+++ app/src/main/java/com/example/zuhaib/blairsbarbershop/DateTimeRequest.java
@@ -1,6 +1,5 @@
 package com.example.zuhaib.blairsbarbershop;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
@@ -10,24 +9,24 @@ public class DateTimeRequest extends StringRequest {
     private static final String DATETIME_REQUEST_URL = "http://blairbbb.epizy.com/DateTime.php";
     private Map<String, String> params;
 
-            public DateTimeRequest(String name, String date, String time, Response.Listener<String> listener){
-                super(Method.POST, DATETIME_REQUEST_URL, listener, null);
-                params = new HashMap<>();
-                params.put("name", name);
-                params.put("date", date);
-                params.put("time", time);
-            }
+        public DateTimeRequest(String name, String date, String time, Response.Listener<String> listener){
+            super(Method.POST, DATETIME_REQUEST_URL, listener, null);
+            params = new HashMap<>();
+            params.put("name", name);
+            params.put("date", date);
+            params.put("time", time);
+        }
 
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> headers = new HashMap<String, String>();
-                headers.put("Cookie", "__test=9b87af7aa785b2dff2dfd57ac40e3d59; expires=Thu, 31-Dec-37 23:55:55 GMT; path=/");
-                return headers;
-            }
+        @Override
+        public Map<String, String> getHeaders() {
+            Map<String, String> headers = new HashMap<String, String>();
+            headers.put("Cookie", "__test=9b87af7aa785b2dff2dfd57ac40e3d59; expires=Thu, 31-Dec-37 23:55:55 GMT; path=/");
+            return headers;
+        }
 
-            @Override
-            public Map<String, String> getParams() {
-                return params;
-            }
+        @Override
+        public Map<String, String> getParams() {
+            return params;
+        }
 
 }
